dac0b050fd281fdc9670e045146cc22a7d028c35
Truffle-11 <truffle11.yt@gmail.com>
2025-11-14 22:49:47 -0700

    Trent fixed some things :D

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-{       
+{
     public float moveSpeed = 5f;
     public float jumpForce = 4.3f;
+    public float health = 3f;
+    public string EnemyTag = "Enemy";
+    public GameObject deadPlayer;
     public Rigidbody2D rb;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -17,23 +20,39 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(EnemyTag))
+        {
+            health--;
+        }
+    }
 
     void Update()
     {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            deadPlayer.SetActive(true);
+        }
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
 
 
         float x = Input.GetAxis("Horizontal");
 
+        Vector2 velocity = rb.velocity;
 
-        Vector2 move = transform.right * x;
-        Vector2 velocity = move * moveSpeed;
-        velocity.y = rb.velocity.y;
+        if (Mathf.Abs(x) > 0.001f)
+        {
+            velocity.x = x * moveSpeed;
+        }
+
         rb.velocity = velocity;
 
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
